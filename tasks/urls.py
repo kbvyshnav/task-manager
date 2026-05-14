da8e8a20dc0d7693d31b24c692aee84b6360bcd8
Vyshnav K B <vyshnavkb648@gmail.com>
2026-05-14 13:51:17 +0530

    Added Django views and URL routing | Created template rendering for task list

---
+++ tasks/urls.py
@@ -0,0 +1,12 @@
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('',views.home,name='home'),
+    path('about/',views.about,name='about'),
+    path('tasks/',views.task_list,name='tasks'),
+
+     #dynamic URL's
+    path('tasks/<int:task_id>/',views.task_detail,name='task_detail'),
+]
--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from datetime import datetime
 
-# Create your views here.
+def home(request):
+    today=datetime.today().strftime("%d-%m-%Y")
+
+    return HttpResponse(
+        f"Welcome to Task Manager — Home Page <br> Today's date is: {today}"
+    )
+
+def about(request):
+    return HttpResponse("<h1>About</h1><p>This is a task manager app built with Django.</p>")
+
+def task_list(request):
+    return render(request, 'tasks/task_list.html')
+
+def task_detail(request, task_id):
+    return HttpResponse(f"Viewing task number {task_id}")
